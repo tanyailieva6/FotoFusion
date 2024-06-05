5d59569a3f691558c0ac059ebf96d6c3857f309e
h.hristov <h.hristov@dynamosoftware.com>
2024-06-05 08:15:02 +0300

    Registers user

---
+++ models/register_user.php
@@ -0,0 +1,40 @@
+<?php
+    include 'DB.php';
+
+    $servername = $_SERVERNAME;
+    $user_server = $_USER_SERVER;
+    $password_server = $_PASSWORD_SERVER;
+    $dbname = $_DBNAME;
+
+    $conn = new mysqli($servername, $user_server, $password_server, $dbname);
+    
+    if($conn -> connect_error)
+    {
+        die("connection failed: ". $conn -> connect_error);
+    }
+    else
+    {
+        $user_array = $_GET['variableName'];
+        $user = explode(",",$user_array);
+
+        $user_username = $user[0];
+        $user_fullname = $user[1];
+        $user_password = $user[2];
+
+        $password_hash = password_hash($user_password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO `users` ( `username`, `fullname`, `role`, `password`) VALUES ('$user_username','$user_fullname','$user_role','$password_hash')";
+        
+        if($conn->query($sql)===TRUE)
+        {
+            echo true;
+        }
+        else
+        {
+            echo false;
+        }
+        
+    }
+
+    $conn->close();
+?>
