a8e85119ed3c9ac729f483cf6e052e1a986addea
h.hristov <h.hristov@dynamosoftware.com>
2024-06-05 08:14:07 +0300

    Initial commit

--- models/initdb.php
+++ models/initdb.php
@@ -1,3 +1,24 @@
 <!--
     Initializes the database
 -->
+
+<?php
+    include 'DB.php';
+
+    $servername = $_SERVERNAME;
+    $user_server = $_USER_SERVER;
+    $password_server = $_PASSWORD_SERVER;
+    $dbname = $_DBNAME;
+
+    $conn = new mysqli($servername, $user_server, $password_server, $dbname);
+    
+    if($conn -> connect_error)
+    {
+        die("connection failed: ". $conn -> connect_error);
+    }
+    else
+    {
+    }
+
+    $conn->close();
+?>
