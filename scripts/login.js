53ab1f94f46b5d84556411c232f093cbbedb5cb8
Antoniya Hristova <karma12451@gmail.com>
2024-06-08 14:13:52 +0300

    Fix

--- scripts/login.js
+++ scripts/login.js
@@ -1,79 +1,61 @@
-const checkUsername = () => {
+function checkUser(event) {
+  event.preventDefault();
+
   const username = document.getElementById("username").value;
-  let isValid = false;
-
-  if (username.length === 0) {
-    document.getElementById("error-username").textContent =
-      "* Username is required";
-    document.getElementById("error-username").style.display = "block";
-  } else if (username.length > 15) {
-    document.getElementById("error-username").textContent =
-      "* Invalid username";
-    document.getElementById("error-username").style.display = "block";
-  } else {
-    document.getElementById("error-username").textContent = "";
-    document.getElementById("error-username").style.display = "none";
-    isValid = true;
-  }
-
-  return isValid;
-};
-
-const checkPassword = () => {
   const password = document.getElementById("password").value;
-  let isValid = false;
 
-  if (password.length === 0) {
-    document.getElementById("error-password").textContent =
-      "* Password is required.";
-    document.getElementById("error-password").style.display = "block";
-  } else {
-    const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,15}$)");
-    isValid = regex.test(password);
+  if (username.length === 0 || password.length === 0) {
+    // Display error messages for empty fields
+    if (username.length === 0) {
+      document.getElementById("error-username").textContent =
+        "* Username is required";
+      document.getElementById("error-username").style.display = "block";
+    } else {
+      document.getElementById("error-username").textContent = "";
+      document.getElementById("error-username").style.display = "none";
+    }
 
-    if (!isValid) {
+    if (password.length === 0) {
       document.getElementById("error-password").textContent =
-        "* Invalid password";
+        "* Password is required.";
       document.getElementById("error-password").style.display = "block";
     } else {
       document.getElementById("error-password").textContent = "";
       document.getElementById("error-password").style.display = "none";
     }
+
+    return; // Exit the function if fields are empty
+  } else {
+    document.getElementById("error-username").textContent = "";
+    document.getElementById("error-username").style.display = "none";
+    document.getElementById("error-password").textContent = "";
+    document.getElementById("error-password").style.display = "none";
   }
 
-  return isValid;
-};
-
-function login(event) {
-  event.preventDefault();
-
-  const isUsernameValid = checkUsername();
-  const isPasswordValid = checkPassword();
-
-  if (isUsernameValid && isPasswordValid) {
-    const userData = {
-      username: document.getElementById("username").value,
-      password: document.getElementById("password").value,
-    };
-
-    fetch("./php/Session.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
+  // If fields are not empty, proceed with fetching data
+  let array = [username, password];
+  console.log(array);
+  fetch("./models/login_user.php?variableName=" + array)
+    .then((response) => response.text())
+    .then((data) => {
+      // Handle the text response
+      if (data) {
+        // Successful authentication
+        console.log(data);
+        console.log("Authentication successful");
+        document.getElementById("error-password").textContent = "";
+        document.getElementById("error-password").style.display = "none";
+      } else {
+        // Error message
+        document.getElementById("error-password").textContent =
+          "Invalid username or password";
+        document.getElementById("error-password").style.display = "block";
+      }
     })
-      .then((response) => response.json())
-      .then((r) => {
-        if (r["success"]) {
-          document.getElementById("errorForm").textContent = "";
-          window.location.href = "./home.html";
-        } else {
-          document.getElementById("errorForm").textContent =
-            "Invalid username or password";
-        }
-      });
-  }
+    .catch((error) => {
+      // Handle fetch error
+      console.error("There was a problem with the fetch operation:", error);
+    });
 }
 
-document.getElementById("login-btn").addEventListener("click", login);
+document.getElementById("login-btn").addEventListener("click", checkUser);
